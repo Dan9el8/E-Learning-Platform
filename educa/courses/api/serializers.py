a40767ceda4e0cda6a3a320bffc14cbafd68ae2a
Dan9el8 <mutetemifloice@gmail.com>
2024-07-22 16:48:04 +0300

    init

---
+++ educa/courses/api/serializers.py
@@ -0,0 +1,23 @@
+from rest_framework import serializers
+from courses.models import Subject, Course
+from django.db.models import Count
+
+class SubjectSerializer(serializers.ModelSerializer):
+    total_courses = serializers.IntegerField()
+    popular_courses =serializers.SerializerMethodField()
+
+    def get_popular_courses(self, obj):
+        courses = obj.courses.annotate(
+            total_students=Count('students')
+            ).order_by('total_students')[:3]
+        return [
+            f'{c.title} ({c.total_students})' for c in courses
+            ]
+    
+    class Meta:
+        model = Subject
+        fields = ['id', 'title', 'slug', 'total_courses', 'popular_courses']
+
+        class CourseSerializer(serializers.ModelSerializer):
+            class Meta:
+                fields = ['id', 'subject', 'title', 'slug', 'overview', 'created', 'owner', 'modules']
---
+++ educa/courses/api/views.py
@@ -0,0 +1,14 @@
+from rest_framework import generics
+from courses.api.pagination import StandardPagination
+from courses.api.serializers import SubjectSerializer
+from django.db.models import Count
+from courses.models import Subject
+
+class SubjectListView(generics.ListAPIView):
+    queryset = Subject.objects.annotate(total_courses=Count('courses'))
+    serializer_class = SubjectSerializer
+    pagination_class = StandardPagination
+
+    class SubjectDetailView(generics.RetriveAPIView):
+        queryset = Subject.objects.annotate(total_courses=Count('courses'))
+        serializer_class = SubjectSerializer
